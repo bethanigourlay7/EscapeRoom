1d0deb794920f1f86492704e4d269ff2800d9a27
bethanigourlay7 <gourlaybethani@gmail.com>
2023-07-26 13:51:18 +0100

    Play and quit buttons

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    
+
+    /**
+     * Script for play button to change to lvl1 scene
+     */
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("QUIT");
+        Application.Quit();
+    }
+
+}
